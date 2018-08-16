626ae7ea12f57f2d99cbb37fe6c69ccde6432c2c
Tomas Toledo <trtoledo1977@gmail.com>
2018-08-16 02:28:21 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,17 +7,17 @@ class Show < ActiveRecord::Base
   end
  def self.lowest_rating
   self.minimum(:rating)
-end
+ end
  def self.least_popular_show
   self.where(rating: Show.minimum(:rating)).first
-end
+ end
  def self.ratings_sum
   self.sum(:rating)
-end
+ end
  def self.popular_shows
-self.where("rating > ?", 5)
+ self.where("rating > ?", 5)
  end
  def self.shows_by_alphabetical_order
   self.order(:name)
-end
+ end
 end
