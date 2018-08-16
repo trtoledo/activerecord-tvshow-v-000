b2898a5500cf71e312c5ac6e6c5dd3f666a92323
Tomas Toledo <trtoledo1977@gmail.com>
2018-08-16 02:05:45 +0000

    automatically backed up by learn

--- Gemfile
+++ Gemfile
@@ -3,5 +3,5 @@ gem 'pry'
 gem "rspec"
 gem "activerecord", "4.2.5"
 gem "sqlite3"
-gem "rake", "10.4.2"
+gem "rake"
 gem "database_cleaner"
---
+++ app/models/show.rb
@@ -0,0 +1,31 @@
+class Show < ActiveRecord::Base
+  def highest_rating
+    Show.maximum("rating")
+  end
+
+  def most_popular_show
+
+
+  end
+
+  def lowest_rating
+
+  end
+
+  def least_popular_show
+
+  end
+
+  def ratings_sum
+
+
+  end
+
+  def popular_shows
+
+  end
+
+  def shows_by_alphabetical_order
+
+  end
+end
